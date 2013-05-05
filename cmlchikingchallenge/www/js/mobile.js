9fdb4f9c179104f83b5cf9627a4bc44365308277
zacktu <member@neqn.net>
2013-05-05 18:37:47 -0400

    I have both my location lat/long and destination lat/long in locSuccess. Now to pass them to Google Maps.

--- cmlchikingchallenge/www/js/mobile.js
+++ cmlchikingchallenge/www/js/mobile.js
@@ -315,14 +315,27 @@ function locError(error) {
  */
 function locSuccess(position) {
 	'use strict';
+	var destinationLatitude,
+		destinationLongitude;
+	var myTrailId = globals.getTrailId();
 	// initialize map with current position and calculate the route
 	// console.log("Entering locSuccess");
 	console.log("locSuccess: latitude = " + position.coords.latitude);
 	console.log("locSuccess: longitude = " + position.coords.longitude);
-	console.log("googleMaps.live.click: this is where we show the map");
-	location.href = "https://maps.google.com/?q=loc:35.4607+-82.3681";
-	location.href = "https://maps.google.com/?q=loc:" + 
-		position.coords.latitude + "+" + position.coords.longitude;
+	console.log("googleMaps.live.click: trailId is " + myTrailId);
+	$.getJSON("trails.json", function(data) {
+		destinationLatitude = data.trails[myTrailId].latitude;
+		destinationLongitude = data.trails[myTrailId].longitude;
+		console.log("locSuccess: destinationLatitude = " + destinationLatitude);
+		console.log("locSuccess: destinationLongitude = " + destinationLongitude);
+		console.log("googleMaps.live.click: this is where we show the map");
+		//location.href = "https://maps.google.com/?q=loc:35.4607+-82.3681";
+		/***
+		location.href = "https://maps.google.com/?q=loc:" + 
+			position.coords.latitude + "+" + position.coords.longitude;
+		****/
+	});
+
 	/****
 	globals.setLatitude(position.coords.latitude);
 	console.log("locSuccess: returned from setLatitude");
@@ -456,8 +469,6 @@ function trailLinkClicked(trailId) {
 } // end linkClickedlistingNumber
 
 $('#googleMaps').live('click', function() {
-	var myTrailId = globals.getTrailId();
-	console.log("googleMaps.live.click: trailId is " + myTrailId);
 	navigator.geolocation.getCurrentPosition(locSuccess, locError);
 	console.log("googleMaps.live.click: returned from getCurrentPosition");
 }); // end #googleMaps.live('click' ...)
